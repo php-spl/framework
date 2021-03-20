db146ea3ef786683238397aab38f02c054a81e92
Thomas Elvin <info@datalaere.dk>
2021-03-20 09:10:42 +0100

    update
    update

--- src/Database/Database.php
+++ src/Database/Database.php
@@ -2,16 +2,58 @@
 
 namespace Web\Database;
 
-use Web\Database\Connection;
+use PDO;
+use PDOException;
 
 class Database
 {
-    protected $db;
+    protected $connection = [
+        'host' => '127.0.0.1',
+        'driver' => 'mysql',
+        'dbname' => '',
+        'username' => 'root',
+        'password' => 'mysql',
+        'charset' => 'utf8',
+        'collation' => 'utf8_unicode_ci',
+        'prefix'    => '',
+    ];
 
-    public function __construct(Connection $connection)
+    public $driver;
+    public $host;
+    public $dbname;
+    public $username;
+    public $password;
+    public $charset;
+    public $collation;
+    public $prefix;
+
+    public function __construct($connection = array())
     {
-        $this->db = $connection;
+        if(!empty($connection)) {
+            $this->connection = $connection;
+        }
 
-        return $this->db;
+        $this->driver = $this->connection['driver'];
+        $this->host = $this->connection['host'];
+        $this->dbname = $this->connection['dbname'];
+        $this->username = $this->connection['username'];
+        $this->password = $this->connection['password'];
+        $this->charset = $this->connection['charset'];
+
+        try {
+            $this->pdo = new PDO(
+                $this->driver . ':host=' . 
+                $this->host . ';dbname=' . 
+                $this->dbname . ';charset=' .
+                $this->charset, 
+                $this->username, 
+                $this->password
+            );
+
+        } catch (PDOException $error) {
+            die($error->getMessage());
+        }
+
+        return $this->pdo;
     }
 }
