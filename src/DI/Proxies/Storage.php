10ed42a6ca88eb54b696c9c3eb6d2dba4c7a5412
Thomas Elvin <info@datalaere.dk>
2021-04-27 09:56:03 +0200

    update

---
+++ src/DI/Proxies/Storage.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Spl\DI\Proxies;
+
+use Spl\DI\Proxy;
+
+class Session extends Proxy
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getProxyAccessor()
+    {
+        return 'filesystem';
+    }
+}
